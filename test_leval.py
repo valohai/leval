3d1351906326b5a537d8984b00f2ef0163d742b7
Aarni Koskela <akx@iki.fi>
2020-12-09 20:04:03 +0200

    Improve test cases

--- test_leval.py
+++ test_leval.py
@@ -1,3 +1,5 @@
+from types import SimpleNamespace
+
 import pytest
 
 from leval.excs import (
@@ -9,40 +11,78 @@ from leval.excs import (
 )
 from leval.simple import simple_eval
 
-values = {"foo": 7, "bar": 8, "muda": "muda", ("nep", "nop"): 10}
-functions = {"abs": abs}
+values = {
+    "foo": 7,
+    "bar": 8,
+    "never": None,
+    "muda": "muda",
+    ("nep", "nop"): 10,
+    "nup": SimpleNamespace(nap=9),
+}
+functions = {
+    "abs": abs,
+}
+
+success_cases = [
+    ("function calls", "abs(foo + 10) > 5", True),
+    ("more complex expressions", "abs(foo + -bar) and bar == +8", True),
+    ("set membership", "bar in {1, 2, 3, 8}", True),
+    ("tuple membership", "bar not in (7, 6, 2)", True),
+    ("concatenation", "'foo' 'bar'", "foobar"),
+    ("AND logic", "foo and not bar", False),
+    ("OR logic", "never or foo", True),
+    ("attribute access via tuple names", "nep.nop + 10", 20),
+    ("complex comparison", "(8, 3) >= (9, 2)", False),
+    ("precedence (no parens here!)", "4 + 3 * 5 + 2", 21),
+    ("precedence (with parens)", "4 + 3 * (5 + 2)", 25),
+]
+
+error_cases = [
+    ("Can't index", "indx[9]", InvalidOperation),
+    ("Can't call arbitrary functions", '__import__("sys")', NoSuchFunction),
+    ("Invalid name", "njep + nop", NoSuchValue),
+    ("Can't create long values", "'stack' * 10000", InvalidOperands),
+    ("Plain old syntax error", "]", SyntaxError),
+    (
+        "Expression is too deeply nested",
+        "(5+(5+(5+(5+(5*3)))))",
+        TooComplex,
+    ),
+    ("Slicing is not allowed", "x[5::3]", InvalidOperation),
+    ("Kwarg calls are not allowed", "abs(num=7)", InvalidOperation),
+    ("Calls to non-names are not allowed", "8(num=7)", InvalidOperation),
+    ("Complex calls are not allowed", "foo.bar(num=7)", InvalidOperation),
+    ("Chained comparisons are not allowed", "5 < bar < 11", InvalidOperation),
+    ("List comprehensions are not allowed", "[a for b in c]", InvalidOperation),
+    ("Set comprehensions are not allowed", "{a for b in c}", InvalidOperation),
+    ("Dict comprehensions are not allowed", "{a: a for b in c}", InvalidOperation),
+    ("Formatting strings is not allowed", "f'foo'", InvalidOperation),
+    ("Constructing dicts is not allowed", "{'d': 8}", InvalidOperation),
+    ("Constructing lists is not allowed", "[1, 2, 3]", InvalidOperation),
+    (
+        "Attribute access via actual objects should fail for safety",
+        "nup.nap + 10",
+        NoSuchValue,
+    ),
+    ("Can't access weird methods off valid names", "abs.__class__", NoSuchValue),
+    ("Arbitrary Python code is not allowed", "if x > a:\n    hello()", SyntaxError),
+]
 
 
 @pytest.mark.parametrize(
-    "case, expected",
-    [
-        ("abs(foo + 10) > 5", True),
-        ("abs(foo + -bar) and bar == 8", True),
-        ("bar in {1, 2, 3, 8}", True),
-        ("'foo' 'bar'", "foobar"),
-        ("foo and not bar", False),
-        ("nep.nop + 10", 20),
-        ("(8, 3) >= (9, 2)", False),
-    ],
+    "description, case, expected",
+    success_cases,
+    ids=[c[0] for c in success_cases],
 )
-def test_success(case, expected):
+def test_success(description, case, expected):
     assert simple_eval(case, values=values, functions=functions) == expected
 
 
 @pytest.mark.parametrize(
-    "case, expected",
-    [
-        ("indx[9]", InvalidOperation),
-        ('__import__("sys")', NoSuchFunction),
-        ("njep + nop", NoSuchValue),
-        ("'stack' * 10000", InvalidOperands),
-        ("]", SyntaxError),
-        ("(5+(5+(5+(5+(5+(5+(5+(5+(5+(5+10))))))))))", TooComplex),
-        ("x[5::3]", InvalidOperation),
-        ("[a for b in c]", InvalidOperation),
-        ("f'foo'", InvalidOperation),
-    ],
+    "description, case, expected",
+    error_cases,
+    ids=[c[0] for c in error_cases],
 )
-def test_error(case, expected):
+def test_error(description, case, expected):
     with pytest.raises(expected):
         simple_eval(case, values=values, functions=functions, max_depth=5)
